52d8f38ccea19f5ca3f8c1d58444dc3cb2d9e7fe
komal-gogi <komalgogi6@gmail.com>
2023-10-23 14:17:17 -0500

    correcting main-test.py file

--- main-test.py
+++ main-test.py
@@ -2,8 +2,9 @@
 from main import Add
 
 def TestAdd():
-    assert Add(2,4) == 6
-    print("Add Function works correctly")
+        assert Add(2,3) == 5
+        assert Add(5,5) == 10
+        print("Add Function works correctly")
 
 if __name__ == '__main__':
-    TestAdd()
+        TestAdd()
